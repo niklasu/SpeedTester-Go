b220dc556c9905d27db79508508f97b472bc54fd
Niklas Enns (ext.) <niklas.enns@consol.de>
2023-10-03 18:41:22 +0200

    made NoopWriter "private" by renaming it to lowercase

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -8,12 +8,12 @@ import (
 func Download(url string) {
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
-	_, _ = io.CopyN(&NoopWriter{}, resp.Body, 10*1000*1000)
+	_, _ = io.CopyN(&noopWriter{}, resp.Body, 10*1000*1000)
 }
 
-type NoopWriter struct {
+type noopWriter struct {
 }
 
-func (n2 *NoopWriter) Write(p []byte) (n int, err error) {
+func (n2 *noopWriter) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
