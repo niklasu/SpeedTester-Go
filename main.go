89601bb501d44e6b0f308b5d4fa573b9b37d0ba3
Niklas Enns (ext.) <niklas.enns@consol.de>
2023-10-14 23:25:19 +0200

    refactor

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"log"
 	"os"
-	"speedtester/configuration"
-	"speedtester/downloader"
 	"time"
 )
 
 func main() {
-	conf, err := configuration.LoadConfig(os.Args)
+	conf, err := LoadConfig(os.Args)
 	if err != nil {
 		log.Println(err.Error())
 		return
@@ -26,6 +24,6 @@ func main() {
 
 func measure(url string, size int64) {
 	start := time.Now()
-	downloader.Download(url, size)
+	Download(url, size)
 	log.Printf("%.2f MBit/s\n", float64(size/(1000000))*8/time.Since(start).Seconds())
 }
